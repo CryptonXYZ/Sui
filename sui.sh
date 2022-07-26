9db1bda49ced7a8e1961282f805313556e6ac443
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-07-26 12:34:24 +0300

    Create sui.sh

---
+++ sui.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 
+
+echo -e '\e[40m\e[91m'
+echo -e '  ____                  _                    '
+echo -e ' / ___|_ __ _   _ _ __ | |_ ___  _ __        '
+echo -e '| |   |  __| | | |  _ \| __/ _ \|  _ \       '
+echo -e '| |___| |  | |_| | |_) | || (_) | | | |      '
+echo -e ' \____|_|   \__  |  __/ \__\___/|_| |_|      '
+echo -e '            |___/|_|                         '
+echo -e '    _                 _                      '
+echo -e '   / \   ___ __ _  __| | ___ _ __ ___  _   _ '
+echo -e '  / _ \ / __/ _  |/ _  |/ _ \  _   _ \| | | |'
+echo -e ' / ___ \ (_| (_| | (_| |  __/ | | | | | |_| |'
+echo -e '/_/   \_\___\__ _|\__ _|\___|_| |_| |_|\__  |'
+echo -e '                                       |___/ '
+echo -e '\e[0m' 
+
+sleep 2
+
+
+apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq
+
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+
+rm -rf /var/sui/db /var/sui/genesis.blob $HOME/sui
+mkdir -p /var/sui/db
+cd $HOME
+git clone https://github.com/MystenLabs/sui.git
+cd sui
+git remote add upstream https://github.com/MystenLabs/sui
+git fetch upstream
+git checkout --track upstream/devnet
+cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
+#curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+sed -i.bak "s/db-path:.*/db-path: \"\/var\/sui\/db\"/ ; s/genesis-file-location:.*/genesis-file-location: \"\/var\/sui\/genesis.blob\"/" /var/sui/fullnode.yaml
+cargo build --release -p sui-node
+mv ~/sui/target/release/sui-node /usr/local/bin/
+sed -i.bak 's/127.0.0.1/0.0.0.0/' /var/sui/fullnode.yaml
+
+echo "[Unit]
+Description=Sui Node
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/sui-node --config-path /var/sui/fullnode.yaml
+Restart=on-failure
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > $HOME/suid.service
+
+mv $HOME/suid.service /etc/systemd/system/
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable suid
+sudo systemctl restart suid
